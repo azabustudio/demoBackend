cf4d0e0a87eff320ad97ffdb50ec311a1c0ea66e
rayjp2010@gmail.com <rayjp2010@gmail.com>
2018-01-24 11:50:04 +0900

    delete dummy

--- queryTest.js
+++ queryTest.js
@@ -13,32 +13,32 @@ http.get('http://localhost:8080/login?username=ray&password=test', (res) => {
     });
 });
 
-http.get('http://localhost:8080/getStatus?category=B', (res) => {
-    res.setEncoding('utf-8');
-    let rawData = '';
-    res.on('data', (chunk) => {
-        rawData += chunk;
-    });
-    res.on('end', () => {
-        try {
-            console.log(rawData);
-        } catch (error) {
-            console.error(error);
-        }
-    });
-});
+// http.get('http://localhost:8080/getStatus?category=B', (res) => {
+//     res.setEncoding('utf-8');
+//     let rawData = '';
+//     res.on('data', (chunk) => {
+//         rawData += chunk;
+//     });
+//     res.on('end', () => {
+//         try {
+//             console.log(rawData);
+//         } catch (error) {
+//             console.error(error);
+//         }
+//     });
+// });
 
-http.get('http://localhost:8080/getClaimList?loginName=ray', (res) => {
-    res.setEncoding('utf-8');
-    let rawData = '';
-    res.on('data', (chunk) => {
-        rawData += chunk;
-    });
-    res.on('end', () => {
-        try {
-            console.log(rawData);
-        } catch (error) {
-            console.error(error);
-        }
-    });
-});
+// http.get('http://localhost:8080/getClaimList?loginName=ray', (res) => {
+//     res.setEncoding('utf-8');
+//     let rawData = '';
+//     res.on('data', (chunk) => {
+//         rawData += chunk;
+//     });
+//     res.on('end', () => {
+//         try {
+//             console.log(rawData);
+//         } catch (error) {
+//             console.error(error);
+//         }
+//     });
+// });
